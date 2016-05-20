19d7971f321139e6e9724d6e89791f4fe74420ae
David Noriega <dnori817@gmail.com>
2016-05-20 00:09:07 -0400

    Completed Milestone #2

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,4 +6,40 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find_by_id(params[:id])
   end  
+
+  def new
+    @restaurant = Restaurant.new   
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to restaurants_path
+    end
+  end
+
+  def edit
+    @restaurant = Restaurant.find_by_id(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find_by_id(params[:id])
+
+    @restaurant.update(restaurant_params)
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find_by_id(params[:id])
+    @restaurant.delete
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :street, :city, :state, :country, :postal_code)
+  end
 end
